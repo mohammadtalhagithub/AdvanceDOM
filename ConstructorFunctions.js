54afb8102a9a05d61dc5d28dac1802be6870d515
mohammadtalhagithub <md.talha226@gmail.com>
2023-05-28 19:47:08 +0530

    Added ConstructorFunctions.js

---
+++ ConstructorFunctions.js
@@ -0,0 +1,22 @@
+// Constructor Function ~ Classes in C#
+// Name of Constructor func must be FirstLetterCapitalised
+// This type of func can be considered similar to a Class in C#, as it represents a blueprint.
+
+/**
+ * @constructor Blueprint for creating a housekeeper object.
+ * @param {*} name takes string value for KeeperName field of the instance
+ * @param {*} age 
+ * @param {*} hasWorkPermit takes boolean values.
+ * @param {*} languages
+ */
+function HouseKeeper (name, age, hasWorkPermit, languages){
+    this.KeeperName = name;
+    this.KeeperAge = age;
+    this.KeeperWorkPermit = hasWorkPermit;
+    this.KeeperLanguages = languages;
+}
+
+// creating an Instance of HouseKeeper
+var houseKeeper1 = new HouseKeeper ("Timmy", 19, true, ["French", "English"]);
+
+console.log(houseKeeper1);
--- SoundByJavaScript.js
+++ SoundByJavaScript.js
@@ -1,31 +1,92 @@
-document.querySelectorAll('.drumBtns')[0].addEventListener('click', function () {
-    var Drum = new Audio('sounds/DrumsAccent.mp3');
+// long method
+/*
+document
+  .querySelectorAll(".drumBtns")[0]
+  .addEventListener("click", function () {
+    var Drum = new Audio("sounds/DrumsAccent.mp3");
     Drum.play();
-    console.log(this);
-    this.style.color = 'white';
-    
-})
+    console.log(this); // console log this element whenever this event is triggered.
+    console.log(this.innerHTML); // console log it's innerHTML.
+    this.style.color = "white";
+  });
 
-document.querySelectorAll(".drumBtns")[1].addEventListener('click', function () {
-    var Bass = new Audio('sounds/BassDrum.wav');
+document
+  .querySelectorAll(".drumBtns")[1]
+  .addEventListener("click", function () {
+    var Bass = new Audio("sounds/BassDrum.wav");
     Bass.play();
     console.log(this);
-})
+    this.style.color = "white";
+  });
 
-document.querySelectorAll(".drumBtns")[2].addEventListener('click', function () {
-    var DrumJokeAccent = new Audio('sounds/DrumJokeAccent.wav');
+document
+  .querySelectorAll(".drumBtns")[2]
+  .addEventListener("click", function () {
+    var DrumJokeAccent = new Audio("sounds/DrumJokeAccent.wav");
     DrumJokeAccent.play();
     console.log(this);
-})
+    this.style.color = "white";
+  });
 
-document.querySelectorAll(".drumBtns")[3].addEventListener('click', function () {
-    var GuitarStroke = new Audio('sounds/GuitarStroke.wav');
+document
+  .querySelectorAll(".drumBtns")[3]
+  .addEventListener("click", function () {
+    var GuitarStroke = new Audio("sounds/GuitarStroke.wav");
     GuitarStroke.play();
     console.log(this);
-})
+    this.style.color = "white";
+  });
 
-document.querySelectorAll(".drumBtns")[4].addEventListener('click', function () {
-    var BassRumble = new Audio('sounds/BassRumble.wav');
+document
+  .querySelectorAll(".drumBtns")[4]
+  .addEventListener("click", function () {
+    var BassRumble = new Audio("sounds/BassRumble.wav");
     BassRumble.play();
     console.log(this);
-})
+    this.style.color = "white";
+  });
+*/
+// using loop and Switch Statements
+let NoOfBtns = document.querySelectorAll(".drumBtns").length;
+
+// loop statement to add EventListener to all element with a class of .drumBtns
+for (let i = 0; i < NoOfBtns; i++) {
+    document.querySelectorAll(".drumBtns")[i].addEventListener("click", function () {
+        /**
+         * @param btnInnerHTML variable to store innerHTML with html class of .drumBtns
+         */
+
+        var btnInnerHTML = this.innerHTML;
+
+        switch (btnInnerHTML) {
+            case "Drum":
+                var audio = new Audio("sounds/DrumsAccent.mp3");
+                audio.play();
+                break;
+
+            case "Bass":
+                var audio = new Audio("sounds/BassDrum.wav");
+                audio.play();
+                break;
+
+            case "Drum Joke":
+                var audio = new Audio("sounds/DrumJokeAccent.wav");
+                audio.play();
+                break;
+
+            case "Guitar":
+                var audio = new Audio("sounds/GuitarStroke.wav");
+                audio.play();
+                break;
+
+            case "Bass Rumble":
+                var audio = new Audio("sounds/BassRumble.wav");
+                audio.play();
+                break;
+
+            default:
+                alert("Method Not implemented for this button...")
+                break;
+        }
+    });
+}
